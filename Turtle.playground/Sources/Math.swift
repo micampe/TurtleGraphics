38a293186898602a30514c739c2a4432490daee3
Michele Campeotto <micampe@micampe.it>
2019-10-07 10:56:17 +0200

    factored out function to find an angle from the sides of a triangle
    # Conflicts:
    #	Turtle.playground/Pages/Words.xcplaygroundpage/Contents.swift

--- Turtle.playground/Sources/Math.swift
+++ Turtle.playground/Sources/Math.swift
@@ -1,12 +1,5 @@
 import CoreGraphics
 
-// MARK: Geometry
-public func hypotenuse(_ a: Double, _ b: Double) -> Double {
-    return (a * a + b * b).squareRoot()
-}
-
-// MARK: Trigonometry
-
 public extension BinaryInteger {
     var toRadians: Double { return Double(self) * .pi / 180 }
 }
@@ -14,6 +7,15 @@ public extension BinaryInteger {
 public extension FloatingPoint {
     var toRadians: Self { return self * .pi / 180 }
     var toDegrees: Self { return self * 180 / .pi }
+}
+
+// MARK: Trigonometry
+public func hypotenuse(_ a: Double, _ b: Double) -> Double {
+    return (a * a + b * b).squareRoot()
+}
+
+public func angle(fromOpposite opposite: Double, adjacent: Double) -> Double {
+    return arctangent(opposite / adjacent)
 }
 
 public func sine(_ degrees: Double) -> Double {
